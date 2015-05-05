3a1070776b58ad0c58abb05fec5850390543eb0c
Don <eencdl@hotmail.com>
2015-05-05 08:38:27 -0700

    add reverse list

---
+++ python/reverseList.py
@@ -0,0 +1,17 @@
+__author__ = 'don'
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        if not head:
+            return head
+        else:
+            return self.listHelper(None,head)
+
+    def listHelper(self, t1, t2):
+        if not t2:
+            return t1
+        tmp = t2.next
+        t2.next = t1
+
+        return self.listHelper(t2, tmp)
